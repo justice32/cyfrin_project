60bfd7e83292f85ad53915bac3175c4f5db410e3
justice32 <vlad3079@gmail.com>
2024-09-05 15:31:46 -0400

    fist stages

---
+++ merkle-airdrop/src/MerkleAirdrop.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.24;
+
+contract MerkleAirdrop {
+    // some list of addresses
+    // allow someone in the list to claim ERC-20 tokens
+    address[] claimers;
+
+    // Allow someone to clain ERC-20 tokens
+}
+ 
